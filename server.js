b9210ce54598cd0e05492b05f5e09dc87d4fe4fa
Petra Bayu Pangestu <petrabayu19@gmail.com>
2023-11-30 22:02:50 +0700

    tambah route /

--- server.js
+++ server.js
@@ -1,64 +1,63 @@
-const bluebird = require('bluebird');
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
+const bluebird = require("bluebird");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 //
 const apiError = require("./utils/Error/apiError");
 const { globalErrHandler } = require("./utils/Error/globalErrHandle");
 
 // access environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // import utils
-const respond = require('./libraries/respond');
-const logger = require('./libraries/logger');
+const respond = require("./libraries/respond");
+const logger = require("./libraries/logger");
 
 // import API Routes
-const eventApiV1 = require('./domains/event/v1/api');
+const eventApiV1 = require("./domains/event/v1/api");
 
 // db start & configs
 try {
-    mongoose.Promise = bluebird;
-    let isConnectedBefore = false;
-    let connect = function () {
-        mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-            useNewUrlParser: true,
-        });
-    };
-    connect();
-    mongoose.connection.on('error', function () {
-        logger.info('Could not connect to MongoDB');
+  mongoose.Promise = bluebird;
+  let isConnectedBefore = false;
+  let connect = function () {
+    mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
     });
-    mongoose.connection.on('disconnected', function () {
-        logger.info('Lost MongoDB connection...');
-        if (!isConnectedBefore) connect();
-    });
-    mongoose.connection.on('connected', function () {
-        isConnectedBefore = true;
-        logger.info('Connection established to MongoDB');
-    });
-    mongoose.connection.on('reconnected', function () {
-        logger.info('Reconnected to MongoDB');
-    });
+  };
+  connect();
+  mongoose.connection.on("error", function () {
+    logger.info("Could not connect to MongoDB");
+  });
+  mongoose.connection.on("disconnected", function () {
+    logger.info("Lost MongoDB connection...");
+    if (!isConnectedBefore) connect();
+  });
+  mongoose.connection.on("connected", function () {
+    isConnectedBefore = true;
+    logger.info("Connection established to MongoDB");
+  });
+  mongoose.connection.on("reconnected", function () {
+    logger.info("Reconnected to MongoDB");
+  });
 
-    // Close the Mongoose connection, when receiving SIGINT
-    process.on('SIGINT', function () {
-        mongoose.connection.close(function () {
-            logger.info('Force to close the MongoDB conection');
-            process.exit(0);
-        });
+  // Close the Mongoose connection, when receiving SIGINT
+  process.on("SIGINT", function () {
+    mongoose.connection.close(function () {
+      logger.info("Force to close the MongoDB conection");
+      process.exit(0);
     });
+  });
 } catch (err) {
-    throw err;
+  throw err;
 }
 
 // app middleware
 const app = express();
 // app.use(express.static('public'));
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 //Set Routes
@@ -66,12 +65,15 @@ const userRouters = require("./domains/users/v1/api");
 const authRouters = require("./domains/auth/v1/api");
 const categoryRouters = require("./domains/category/v1/api");
 const postRouters = require("./domains/post/v1/api");
-const commentRouters = require("./domains/comment/v1/api")
-const eventRouters = require("./domains/event/v1/api")
-const programRouters = require("./domains/program/v1/api")
-const lessonRouters = require("./domains/lessons/v1/api")
+const commentRouters = require("./domains/comment/v1/api");
+const eventRouters = require("./domains/event/v1/api");
+const programRouters = require("./domains/program/v1/api");
+const lessonRouters = require("./domains/lessons/v1/api");
 
 // Use Routes
+app.use("/", (req, res) => {
+  res.send("welcome to the nutrikid server");
+});
 app.use("/api/users", userRouters);
 app.use("/api/auth", authRouters);
 app.use("/api/categories", categoryRouters);
@@ -83,21 +85,21 @@ app.use("/api/lesson", lessonRouters);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    logger.info("NOT FOUND!");
-    respond.responseNotFound(res);
+  logger.info("NOT FOUND!");
+  respond.responseNotFound(res);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    logger.info(err);
-    respond.responseError(res);
+  logger.info(err);
+  respond.responseError(res);
 });
 
 // finalize
 module.exports = app;
 
 // listen
-const port = process.env.PORT
-app.listen(port, function() {
-    console.log(`Server is running in port : ${ port }`)
-})
+const port = process.env.PORT;
+app.listen(port, function () {
+  console.log(`Server is running in port : ${port}`);
+});
